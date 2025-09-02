7eb86e1e7857e990bb31f9d7df68f8c82208ec32
Julia Fasick <juli4fasick@gmail.com>
2025-09-02 19:45:28 -0400

    adds EquilateralTriangle.kt

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,9 @@ fun main() {
         Circle("Circle1"),
         Circle("Circle2"),
         Triangle("Triangle1"),
-        Triangle("Triangle2")
+        Triangle("Triangle2"),
+        EquilateralTriangle("EquilateralTriangle1"),
+        EquilateralTriangle("EquilateralTriangle2")
     )
     for (shape in shapes) {
         shape.setDimensions()
